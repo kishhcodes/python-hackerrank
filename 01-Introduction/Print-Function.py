2fc236644453f10a62d5f64816356cab202140d2
Krishnaa S <113958356+kishhcodes@users.noreply.github.com>
2024-10-12 09:32:13 +0530

    Create Print-Function.py

---
+++ 01-Introduction/Print-Function.py
@@ -0,0 +1,6 @@
+#Problem: https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+if __name__ == '__main__': 
+    n = int(input())      #Solution starts here
+    for i in range(0,n):
+        print (i+1,end='',sep='\n')
